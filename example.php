310ebaad3e924fdb8d76fe9a4483d191adf32c20
Niklas Riekenbrauck <nikriek@users.noreply.github.com>
2014-02-16 16:26:31 +0100

    Update example.php

--- example.php
+++ example.php
@@ -11,8 +11,8 @@ $push = new PushCo(array(
 try {
   	$push->sendArticleAndImage(
   		"Message sent",
-  		"http://press.push.co/70303-ifttt-and-push-co-a-match-made-in-heaven",
-  		"https://d21buns5ku92am.cloudfront.net/38491/images/107905-d8ddae8e-db73-41b7-a460-73667dc1c703-iphone5screen5-1-large-1378978123.PNG");
+  		"http://example.de/article",
+  		"http://example.de/img.jpg");
   	$push->sendUrl("Url sent","http://nikriek.de");
 	$push->sendLocation("Location sent",13.4113999,52.5234051);
 } catch (Exception $e) {
