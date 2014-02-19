b855b6b785e69e478cf62686a568ee6a34773116
Niklas <nikriek@me.com>
2014-02-19 17:56:51 +0100

    - began with a code documentation. - all kind of error now throw a specific exception

--- php/push.php
+++ php/push.php
@@ -1,9 +1,19 @@
 <?php
+	/**
+	* PushCo
+	* 
+	* The PushCo class is a wrapper for Push.co's API, which can POST notifications to their iPhone-App
+	*/
 	class PushCo {
 		private $config = array();
 
 		const API_URL = 'https://api.push.co/1.0/push';
 
+		/**
+		* The constructor needs an assoziative array with your app's API-Key and -Secret
+		*
+		* @param configuration array
+		*/
 		public function __construct($configArray) {
 			$this->config = $configArray;
 		}
@@ -63,14 +73,20 @@
 		}
 
 		private function sendRequest($params,$notificationType = null) {
-			if ($notificationType != null) {
-				$params['notification_type'] = $notificationType;
-			}
+			//An empty config array throws an exception
 			if (empty($this->config)) {
 				throw new Exception("Missing connection credentials", 1);
 			}
+
+			//Only append notification_type if given in parameters
+			if ($notificationType != null) {
+				$params['notification_type'] = $notificationType;
+			}
+
+			//Merge all paramaters
 			$data = array_merge($this->config,$params);
 
+			//Create curl-object with needed params
 			$fields = '';
 	   		foreach($data as $key => $value) { 
 	      		$fields .= $key . '=' . $value . '&'; 
@@ -85,16 +101,22 @@
 	   		curl_setopt($post, CURLOPT_RETURNTRANSFER, 1);
 
 	   		$result = curl_exec($post);
-	   		if(curl_exec($post) === false)
-			{
+	   		if(curl_exec($post) === false) {
+	   			//Handle curl error
 				curl_close($post);
     			throw new Exception(curl_error($post), 2);
 			} else if (curl_getinfo($post, CURLINFO_HTTP_CODE) == 401) {
+				//Throw exception in case of invalid credentials
 				curl_close($post);
 				throw new Exception("Invalid credentials", 3);
 			}
 	   		curl_close($post);
-	   		return $result;
+
+	   		//Handle non-success of API-call
+	   		$json = json_decode($result);
+	   		if ($json['success'] != true) {
+	   			throw new Exception($json['message'], 4);	
+	   		}
 		}
 	}
 ?>
