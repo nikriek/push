6db17cf13d3f39246fcbe224da1b01037bd0cae8
Niklas <nikriek@me.com>
2014-02-20 19:01:23 +0100

    Finished python library

--- python/example.py
+++ python/example.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pushco import PushCo
+
 def main():
-  	print 'Test'
+  	config = {
+  		"api_secret" : "api_secret",
+  		"api_key" : "api_key"
+  	}
+  	push = PushCo(config)
+  	push.sendUrl("Url","https://google.de")
+  	print 'Notification successfully pushed!'
 
 if __name__ == "__main__":
   main()
--- python/pushco/__init__.py
+++ python/pushco/__init__.py
@@ -15,11 +15,10 @@ class PushCo(object):
 
 	def __sendRequest(self,params,notificationType=None):
 		if self.config == None:
-			raise Exception("Configuration data is missing")
+			raise Exception("Configuration data is missing.")
 
 		if notificationType != None:
 			params.update(notificationType)
-
 		params.update(self.config)
 		data = urllib.urlencode(params)
 		req = urllib2.Request(self.API_URL, data)
@@ -28,10 +27,54 @@ class PushCo(object):
 		except urllib2.HTTPError, e:
 			if e.code == 401:
 				raise Exception("You are not authorized.")
+			else:
+				raise Exception("Unkown error")
 			
 	def sendMessage(self,message,notificationType=None):
 		params = {
 			"message": message,
 			"view_type": 0
+		}
+		self.__sendRequest(params,notificationType)
+
+	def sendArticle(self,message,article,notificationType=None):
+		params = {
+			"message": message,
+			"view_type": 0,
+			"article":article
+		}
+		self.__sendRequest(params,notificationType)
+
+	def sendImage(self,message,image,notificationType=None):
+		params = {
+			"message": message,
+			"view_type": 0,
+			"image" : image
+		}
+		self.__sendRequest(params,notificationType)
+
+	def sendArticleAndImage(self,message,article,image,notificationType=None):
+		params = {
+			"message": message,
+			"view_type": 0,
+			"article": article,
+			"image" : image
+		}
+		self.__sendRequest(params,notificationType)
+
+	def sendUrl(self,message,url,notificationType=None):
+		params = {
+			"message": message,
+			"view_type": 1,
+			"url": url
+		}
+		self.__sendRequest(params,notificationType)
+
+	def sendLocation(self,message,longitude,latitude,notificationType=None):
+		params = {
+			"message": message,
+			"view_type": 2,
+			"longitude": str(longitude),
+			"latitude" : str(latitude)
 		}
 		self.__sendRequest(params,notificationType)
