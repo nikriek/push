c0309d2abdfa5bf45fa06656f770dcd060077326
Niklas <nikriek@me.com>
2014-02-20 14:54:10 +0100

    added stuff to PushCo module in python

---
+++ python/pushco/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib      
+import urllib2
+
+class PushCo(object):
+	"""docstring for PushCo"""
+
+	API_URL = "https://api.push.co/1.0/push"
+	
+	def __init__(self, config):
+		super(PushCo, self).__init__()
+		self.config = config
+
+	def __sendRequest(self,params,notificationType=None):
+		if self.config == None:
+			raise Exception("Configuration data is missing")
+
+		if notificationType != None:
+			params.update(notificationType)
+
+		params.update(self.config)
+		data = urllib.urlencode(params)
+		req = urllib2.Request(self.API_URL, data)
+		try:
+			response = urllib2.urlopen(req)
+		except urllib2.HTTPError, e:
+			if e.code == 401:
+				raise Exception("You are not authorized.")
+			
+	def sendMessage(self,message,notificationType=None):
+		params = {
+			"message": message,
+			"view_type": 0
+		}
+		self.__sendRequest(params,notificationType)
